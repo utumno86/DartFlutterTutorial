c7c889bf5f17874273f26c002e22f7b924e76adb
Michael Nash <utumno86@gmail.com>
2020-03-23 16:38:39 -0400

    initial cards script

---
+++ cards.dart
@@ -0,0 +1,46 @@
+void main() {
+  var deck = new Deck();
+  deck.shuffle();
+  print(deck.cardsWithSuit('Clubs'));
+}
+ 
+class Deck {
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(suit, rank);
+        cards.add(card);
+      }
+    }
+  }
+
+  toString() {
+    return cards.toString();
+  }
+
+  shuffle() {
+    cards.shuffle();
+  }
+
+  cardsWithSuit(String suit) {
+    return cards.where((card) {
+      return card.suit == suit;
+    });
+  }
+}
+
+class Card {
+  String suit;
+  String rank;
+
+  Card(this.suit, this.rank);
+
+  toString() {
+    return '$rank of $suit';
+  }
+}
