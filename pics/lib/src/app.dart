27f91d558f807e91a7eec6f843d1d594c1f7efb4
Michael Nash <utumno86@gmail.com>
2020-03-30 16:24:41 -0400

    making this flutter app stateful

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -3,15 +3,27 @@ import 'package:flutter/material.dart';
 
 // Create a class that will be our custom widget
 // This class must extend the 'StatelessWidget' base class
-class App extends StatelessWidget {
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  
   // Must define a 'build' method that returns
   // the widgets that *this* widget will show
   Widget build(context){
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            print('Hi there!');
+            setState(() {
+              counter += 1;
+            });
           } ,
           child: Icon(Icons.add),
         ),
