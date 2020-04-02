a2e457361f3ea66b190dd84fbf3a99a6d60c6e05
Michael Nash <utumno86@gmail.com>
2020-04-02 16:30:16 -0400

    dealing with the api response

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,5 +1,10 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+
+import 'dart:convert';
+
+import 'models/image_model.dart';
 
 // Create a class that will be our custom widget
 // This class must extend the 'StatelessWidget' base class
@@ -12,6 +17,12 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+
+  void fetchImage() async {
+    counter ++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/500/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+  }
   
   // Must define a 'build' method that returns
   // the widgets that *this* widget will show
@@ -20,11 +31,7 @@ class AppState extends State<App> {
       home: Scaffold(
         body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          } ,
+          onPressed: fetchImage,
           child: Icon(Icons.add),
         ),
         appBar: AppBar(
