e042a96a94c3a8d48b572553752699c59d194ef1
Michael Nash <utumno86@gmail.com>
2020-04-03 17:19:50 -0400

    refactor and adding the title

--- pics/lib/src/widgets/image_list.dart
+++ pics/lib/src/widgets/image_list.dart
@@ -10,17 +10,29 @@ class ImageList extends StatelessWidget {
   Widget build(context) {
     return ListView.builder(
       itemBuilder: (context, int index) {
-        return Container(
+       return buildImage(images[index]);
+      },
+      itemCount: images.length,
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+     return Container(
           decoration: BoxDecoration(
             border: Border.all(color: Colors.grey) 
           ),
           padding: EdgeInsets.all(20.0),
           margin: EdgeInsets.all(20.0),
-          child: Image.network(images[index].url)
+          child: Column(
+            children: <Widget>[
+              Padding(
+                child: Image.network(image.url),
+                padding: EdgeInsets.only(bottom: 20.0)
+              ),
+              Text(image.title)
+            ],
+          )
         );
-      },
-      itemCount: images.length,
-    );
   }
 }
 
