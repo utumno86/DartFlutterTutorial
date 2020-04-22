45cb5a61b144bc0dce98aaf7d248b624f17682fd
Michael Nash <utumno86@gmail.com>
2020-04-22 14:58:07 -0400

    bare bones setup

---
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [],
+      )
+    );
+  }
+}
