f9a3b4ef33fd03d1f07523984d46c0344fb2015d
Michael Nash <utumno86@gmail.com>
2020-04-22 15:09:53 -0400

    basic UI for login screen

--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -5,8 +5,41 @@ class LoginScreen extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
-        children: [],
+        children: [
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
       )
     );
   }
+}
+
+Widget emailField() {
+   return TextField(
+    keyboardType: TextInputType.emailAddress,
+    decoration: InputDecoration(
+      hintText: 'you@example.com',
+      labelText: 'Email Address'
+    ),
+  );
+}
+
+Widget passwordField() {
+   return TextField(
+    obscureText: true,
+    decoration: InputDecoration(
+      hintText: 'password',
+      labelText: 'Password'
+    ),
+  );
+}
+
+Widget submitButton() {
+  return RaisedButton(
+    child: Text('Log In'),
+    color: Colors.blue,
+    onPressed: () {},
+  );
 }
