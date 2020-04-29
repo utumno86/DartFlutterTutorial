8578240bbf2901949b3b1594d7ca7cc27ef4d6e3
Michael Nash <utumno86@gmail.com>
2020-04-29 12:44:16 -0400

    added the validators to the bloc code

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
 
-class Bloc {
+import 'package:login_bloc/src/blocs/validators.dart';
+
+class Bloc with Validators {
   final _email = StreamController<String>();
   final _password = StreamController<String>();
 
   // Add data to the stream
-  Stream<String> get email => _email.stream;
-  Stream<String> get password => _password.stream;
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
 
   // Change data
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
 }
