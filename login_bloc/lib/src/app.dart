86592a26bab63aeae9c390edd220796a0746b2e0
Michael Nash <utumno86@gmail.com>
2020-05-26 14:04:41 -0400

    bloc login page, refactored to use scoped blocs

--- login_bloc/lib/src/app.dart
+++ login_bloc/lib/src/app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:login_bloc/src/blocs/provider.dart';
 import 'screens/login_screen.dart';
 
 class App extends StatelessWidget {
   build(context){
-    return MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
-      )
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        )
+      ),
     );
   }
 }
