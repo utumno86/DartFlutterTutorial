e4ecabda2ce34169d011dd921dece5bfc03fe006
Michael Nash <utumno86@gmail.com>
2020-06-16 15:46:58 -0400

    initial cat animations

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -6,7 +6,29 @@ class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0)
+      .animate(
+        CurvedAnimation(
+          parent: catController,
+          curve: Curves.easeIn,
+        ),
+      );
+
+    catController.forward();
+  }
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
@@ -18,7 +40,16 @@ class HomeState extends State<Home> {
   }
 
   Widget buildAnimation() {
-    return Cat();
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
   }
 }
 
