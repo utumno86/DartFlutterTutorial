5b631e32f7cf2f1be1752311c4cae67077fcf68e
Michael Nash <utumno86@gmail.com>
2020-06-29 16:26:09 -0400

    build out the left flap

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
 import '../widgets/cat.dart';
 
 class Home extends StatefulWidget {
@@ -73,10 +75,17 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
   }
 
   Widget buildLeftFlap(){
-    return Container(
-      height: 10.0,
-      width: 125.0,
-      color: Colors.red
+    return Positioned(
+      left: 3.0,
+      child: Transform.rotate(
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown
+        ),
+        angle: pi * 0.6,
+        alignment: Alignment.topLeft,
+      ),
     );
   }
 
